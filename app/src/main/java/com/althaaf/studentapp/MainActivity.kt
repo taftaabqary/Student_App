7a76e59d9dbbde6cdf70072adc21540e414d82da
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2024-01-22 07:59:24 +0700

    First Commit: XML Implementation - Login and Student List

---
+++ app/src/main/java/com/althaaf/studentapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.althaaf.studentapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fo_activity_main)
+    }
+}
